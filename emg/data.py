92e52301d0f12d8ac3d837641b1593f8ff86fe7f
Patrick Kalmbach <Patrick.Kalmbach@live.com>
2015-07-27 19:52:58 +0200

    one hot multi dimensional

--- emg/data.py
+++ emg/data.py
@@ -717,24 +717,27 @@ def padzeros(sequences, as_list=True, front=False):
         padded = tmp
     return padded
 
-def one_hot(labels, hot_label, hot_value=1, cool_value=0):
-    """ Transforms a list of labels into *one hot* coding scheme
+def one_hot(labels, hot_labels, hot_value=1, cool_value=0):
+    """ Transforms a list of labels into *one hot* coding scheme. Return value
+        will be an array of `len(labels) x len(hot_labels)`.
+
+        The order of labels in `hot_labels` determines the one hot coding
+        scheme.
 
         Args:
             labels (List): List of labels to be transformed into one hot scheme
-            hot_label (object): Target label
+            hot_labels (List): Target labels
             hot_value (int, optional): Value for the target label
             cool_value (int, optional): Value for all labels being not target
                 label
 
         Returns:
-            one_hot, List of ints
+            array
     """
-    one_hot = []
-    for label in labels:
-        if label == hot_label:
-            one_hot.append(hot_value)
-        else:
-            one_hot.append(cool_value)
+    one_hot = np.repeat(cool_value, len(hot_labels) * len(labels)).reshape(
+            len(labels), len(hot_labels)
+            )
+    for i in range(len(labels)):
+        one_hot[i, hot_labels.index(labels[i])] = hot_value
     return one_hot
 
