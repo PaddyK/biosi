d1d965bf0b5ebe35df8ab8a57f3302424492bd41
Patrick Kalmbach <Patrick.Kalmbach@live.com>
2015-08-11 13:20:40 +0200

    general regression model

--- online/regression.py
+++ online/regression.py
@@ -7,13 +7,41 @@ class LinReg(object):
         to ML Lecture
     """
 
-    def __init__(self, dim_in, dim_out, transform):
-        self._W = np.random.randn((dim_in, dim_out))
-        """ weight matrix, columns as features, rows as classes """
-        self._phi=transform
+    def __init__(self, dim_in, dim_out, batchsize, transform, polylenght):
+        """ Initializes Object
 
-    def phi(self, X):
-        return self._phi(X)
+            Args:
+                dim_in (int): Dimensionality of input
+                dim_out (int): Dimensionality of output
+                batchsize (int): Size of batch
+                transform (List): List of transformation functions for design
+                    matrix
+                polylenght (int): Length of polynomial
+
+            Note:
+                polylength has to be a multiple of legnth of transform
+
+            Raises:
+                AssertionError if polylength not multiple of length of transform
+        """
+        wholes, remainder = divmod(polylenght, len(trasnform))
+        assert remainder == 0, 'polylength not multiple of length of ' + \
+                '`transform` (online.regression.LinReg.__init__)'
+
+        self._W = np.random.randn(dim_in, dim_out)
+        """ weight matrix, columns as features, rows as classes """
+
+        repeat = polylenght / len(transform)
+        self._PSI = np.tile(np.tile(transform, repeat), batchsize).reshape(batchsize, -1)
+        self._PSI = np.row_stack((
+            lambda x: x,
+            self._PSI
+            ))
+        """ Design matrix with first element being bias and the remainder
+            activation functions for each element of the minibatch
+        """
+        self._transform = np.vectorize(lambda PSI, X: PSI(X), otypes=[object])
+        """ Apply design matrix to minibatch """
 
     def grad(self, X, Z):
         """ Calculates gradient for minibatch
@@ -26,11 +54,14 @@ class LinReg(object):
                 array of shape (classes, features)
         """
 
-        step = np.dot(self._W.T, self.phi(X).T)
+        print 'LinReg.grad W.T: {}, phi(X).T: {}'.format((self._W.T).shape, (self.phi(X).T).shape)
+        X_head = self._transform(self._PSI, X)
+        step = np.dot(self._W.T, X_head.T)
         # (classes x features) * (features x samples) --> classes x samples
         step = Z - step.T
         # samples x classes - samples x classes --> samples x classes
-        step = np.dot(step.T, self.phi(X).T)
+        print 'LinReg.grad step.T: {}, phi(X).T: {}'.format((step.T).shape, (self.phi(X).T).shape)
+        step = np.dot(step.T, X_head.T)
         # classes x samples * samples x features --> classes x features
         return step.T
         # features x classes
@@ -56,7 +87,8 @@ class LinReg(object):
             Returns:
                 prediction, numpy.ndarray, rowwise
         """
-        prediction = np.dot(self._W.T, self.phi(X).T)
+        X_head = self._transform(X)
+        prediction = np.dot(self._W.T, X_head.T)
         # classes x features * features x samples --> classes x samples
         return prediction.T
         # samples x classes
