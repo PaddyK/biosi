fad5ad829be6b8f26ffb2a0907fee8042e09f3a1
Patrick Kalmbach <Patrick.Kalmbach@live.com>
2015-08-13 15:50:25 +0200

    change to model

--- online/regression.py
+++ online/regression.py
@@ -7,41 +7,43 @@ class LinReg(object):
         to ML Lecture
     """
 
-    def __init__(self, dim_in, dim_out, batchsize, transform, polylenght):
+    def __init__(self, dim_in, dim_out, dim_basis, basis_fcts):
         """ Initializes Object
 
             Args:
                 dim_in (int): Dimensionality of input
                 dim_out (int): Dimensionality of output
-                batchsize (int): Size of batch
-                transform (List): List of transformation functions for design
-                    matrix
-                polylenght (int): Length of polynomial
+                dim_basis (int): Dimensionality of vector of basis functions
+                basis_fcts (array): column array of transfer functions (first
+                    element has to be one) with as many rows as dim_in
 
             Note:
-                polylength has to be a multiple of legnth of transform
+                The basis function will be given a matrix where samples are
+                stored rowwise. The basis function must return a Matrix with
+                the same number of rows, columns may differ
 
-            Raises:
-                AssertionError if polylength not multiple of length of transform
+            Available Basis Functions:
+                tanh: Apply hyperbolic tangent to each element of x_{i}
+                sigmoid: Apply sigmoid to each element of x_{i}
+                polynomial: Raise each feature to power according to position
+                    that is first feature to power 0, nth feature to power n-1
         """
-        wholes, remainder = divmod(polylenght, len(trasnform))
-        assert remainder == 0, 'polylength not multiple of length of ' + \
-                '`transform` (online.regression.LinReg.__init__)'
+        self._W = np.random.randn(dim_basis, dim_out)
+        """ weight matrix, columns as features due to activation functions, rows as classes """
+        self._psi = self._register_basis_function(basis_fcts)
+        """ Apply respective transfer function to each feature """
 
-        self._W = np.random.randn(dim_in, dim_out)
-        """ weight matrix, columns as features, rows as classes """
-
-        repeat = polylenght / len(transform)
-        self._PSI = np.tile(np.tile(transform, repeat), batchsize).reshape(batchsize, -1)
-        self._PSI = np.row_stack((
-            lambda x: x,
-            self._PSI
-            ))
-        """ Design matrix with first element being bias and the remainder
-            activation functions for each element of the minibatch
+    def _register_basis_function(self, basis_fcts):
+        """ Checks if one of supported keywoards and creates fct accordingly
         """
-        self._transform = np.vectorize(lambda PSI, X: PSI(X), otypes=[object])
-        """ Apply design matrix to minibatch """
+        if basis_fcts == 'tanh':
+            return np.tanh
+        elif basis_fcts == 'sigmoid':
+            return lambda X: np.divide(1., np.add(1., np.exp(-X)))
+        elif basis_fcts == 'polynomial':
+            return lambda X, order: np.power(X, order)
+        else:
+            return basis_fcts
 
     def grad(self, X, Z):
         """ Calculates gradient for minibatch
@@ -53,18 +55,21 @@ class LinReg(object):
             Returns:
                 array of shape (classes, features)
         """
+        X_ = self._psi(X)
+        g = np.mean(np.dot(np.dot(X_, W).T, X_), axis=1)
+        # [(n x db)*(db x dz)] --> (n, dz) --> (dz, n)*(n, db) --> (dz, n) --> (dz, 1)
+        return g
 
-        print 'LinReg.grad W.T: {}, phi(X).T: {}'.format((self._W.T).shape, (self.phi(X).T).shape)
-        X_head = self._transform(self._PSI, X)
-        step = np.dot(self._W.T, X_head.T)
-        # (classes x features) * (features x samples) --> classes x samples
-        step = Z - step.T
-        # samples x classes - samples x classes --> samples x classes
-        print 'LinReg.grad step.T: {}, phi(X).T: {}'.format((step.T).shape, (self.phi(X).T).shape)
-        step = np.dot(step.T, X_head.T)
-        # classes x samples * samples x features --> classes x features
-        return step.T
-        # features x classes
+    def loss(self, X, Z):
+        """ Let Y be the prediction
+        """
+        Y = self.predict(X, Z)
+        # (n, dz)
+        D = np.subtract(Z, Y)
+        # (n, dz)
+        loss = np.mean(np.sum(np.power(D, 2), axis=0))
+        # (n, dz) --> (dz,) --> (1,)
+        return np.mean(np.dot(self.predict(X,Z).T, Z))
 
     def train(self, X, Z, alpha=0.01):
         """ Performs online learning step using gradient descend on a minibatch
@@ -75,21 +80,20 @@ class LinReg(object):
                     one row one sample)
                 alpha (float, optional): Learning Rate
         """
-        W_head = self.grad(X, Z)
-        self._W = self.W - alpha * W_head
+        grad = self.grad(X, Z)
+        self._W = self.W - alpha * grad
 
     def predict(self, X):
         """ Predicts values for given dataset
 
             Args:
-                X (numpy.ndarray): Data, rowwise
+                X (numpy.ndarray): Data
 
             Returns:
-                prediction, numpy.ndarray, rowwise
+                prediction, numpy.ndarray
         """
-        X_head = self._transform(X)
-        prediction = np.dot(self._W.T, X_head.T)
-        # classes x features * features x samples --> classes x samples
-        return prediction.T
-        # samples x classes
+        """ Predicts value for a single vector
+        """
+        Y = np.dot(self._psi(X), W)
+        return Y
 
